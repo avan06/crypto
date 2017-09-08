cd54beeacd19af49abd90cbe01961edf29c75a7b
avan <sakaja_218@yahoo.com.tw>
2017-09-09 00:09:11 +0800

    Fix import pkg wrong issue.

--- salsa20/salsa20.go
+++ salsa20/salsa20.go
@@ -24,7 +24,7 @@ package salsa20 // import_ "golang.org/x/crypto/salsa20"
 // TODO(agl): implement XORKeyStream12 and XORKeyStream8 - the reduced round variants of Salsa20.
 
 import (
-	"crypto/salsa20/salsa"
+	"github.com/avan06/crypto/salsa20/salsa"
 )
 
 // XORKeyStream crypts bytes from in to out using the given key and nonce. In
